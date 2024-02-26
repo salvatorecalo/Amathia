5b68c2df75f89fd6e33549dec8850fcbf36e99c7
salvatorecalo <salvatorecalo4@gmail.com>
2024-02-26 09:23:02 +0100

    feat: add shared preferiencies logic so after you see one time on boarding screen it will not be shown anymore

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:amathia/src/costants/costants.dart';
 import 'package:amathia/src/screens/onboard/onboard.dart';
+import 'package:amathia/src/screens/register_page/register_page.dart';
+import 'package:amathia/src/screens/welcome_page/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  isviewed = prefs.getInt('onBoard');
+  await Supabase.initialize(
+    url: 'https://yyiqjaekqhmdyxfcmgts.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5aXFqYWVrcWhtZHl4ZmNtZ3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg4OTY2ODksImV4cCI6MjAyNDQ3MjY4OX0.bg_SU8zQE9GeUAlqO4N68mKelzn-F27934gjeUAcs54',
+  );
   runApp(const MyApp());
 }
 int? isviewed;
@@ -23,7 +34,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: isviewed != 0 ? const OnBoard() : const Text("Home"),
+      home: isviewed != 0 ? const OnBoard() : const WelcomePage(),
     );
   }
 }
