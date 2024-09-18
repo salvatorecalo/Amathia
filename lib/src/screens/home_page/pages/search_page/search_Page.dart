13d7f72337cb4bc62f4713d7723557d8d58e208a
Salvo <dafder45678@gmail.com>
2024-09-18 14:20:41 +0200

    fix: correct display slider title

--- lib/src/screens/home_page/pages/search_page/search_Page.dart
+++ lib/src/screens/home_page/pages/search_page/search_Page.dart
@@ -1,29 +1,29 @@
 import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:amathia/src/screens/home_page/pages/search_page/widget/cards/card/city_card.dart';
 import 'package:amathia/src/screens/home_page/pages/search_page/widget/cards/card/monument_card.dart';
 import 'package:amathia/src/screens/home_page/pages/search_page/widget/cards/card/nature_card.dart';
 import 'package:amathia/src/screens/home_page/pages/search_page/widget/cards/card/recipe_card.dart';
 import 'package:amathia/src/screens/home_page/pages/search_page/widget/category_buttons.dart';
 import 'package:amathia/src/screens/home_page/pages/search_page/widget/searchbar.dart';
-import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension LocalizationExtension on AppLocalizations {
   String? getString(String key) {
     final Map<String, String> localizedStrings = {
-      'recipesTitle1': recipesTitle1,
-      'recipesTitle2': recipesTitle2,
-      'recipesTitle3': recipesTitle3,
-      'boroughsTitle1': boroughsTitle1,
-      'boroughsTitle2': boroughsTitle2,
-      'boroughsTitle3': boroughsTitle3,
-      'monumentsTitle1': monumentsTitle1,
-      'monumentsTitle2': monumentsTitle2,
-      'monumentsTitle3': monumentsTitle3,
-      'natureTitle1': natureTitle1,
-      'natureTitle2': natureTitle2,
-      'natureTitle3': natureTitle3,
+      'ricetteTitle1': ricetteTitle1,
+      'ricetteTitle2': ricetteTitle2,
+      'ricetteTitle3': ricetteTitle3,
+      'borghiTitle1': borghiTitle1,
+      'borghiTitle2': borghiTitle2,
+      'borghiTitle3': borghiTitle3,
+      'monumentiTitle1': monumentiTitle1,
+      'monumentiTitle2': monumentiTitle2,
+      'monumentiTitle3': monumentiTitle3,
+      'naturaTitle1': naturaTitle1,
+      'naturaTitle2': naturaTitle2,
+      'naturaTitle3': naturaTitle3,
     };
 
     return localizedStrings[key];
@@ -52,47 +52,50 @@ class _SearchPageState extends State<SearchPage> {
         final data = response as List<dynamic>;
 
         final widgetGenerated = data.map<Widget>((e) {
+          final title = e['title'] ?? localizations.titleNotAvailable;
+          final image = client.storage.from(tableName).getPublicUrl(e['image']);
+          final description =
+              e['description'] ?? localizations.descriptionNotAvailable;
+          final location = e['location'] ?? localizations.titleNotAvailable;
+
           if (tableName == "Ricette") {
             return Container(
               margin: const EdgeInsets.only(right: 10),
               child: RecipeCard(
-                title: e['title'] ?? localizations.titleNotAvailable,
-                image: client.storage.from(tableName).getPublicUrl(e['image']),
-                description:
-                    e['description'] ?? localizations.descriptionNotAvailable,
+                title: title,
+                image: image,
+                description: description,
                 time: e['time'] ?? 2,
                 peopleFor: e['peopleFor'] ?? 1,
-                ingredients: List<String>.from(e['ingredients']),
+                ingredients: List<String>.from(e['ingredients'] ?? []),
               ),
             );
           } else if (tableName == "Monumenti") {
             return Container(
               margin: const EdgeInsets.only(right: 10),
               child: MonumentsCard(
-                location: e['location'] ?? localizations.titleNotAvailable,
-                image: client.storage.from(tableName).getPublicUrl(e['image']),
-                title: e['title'] ?? localizations.titleNotAvailable,
-                description:
-                    e['description'] ?? localizations.descriptionNotAvailable,
+                location: location,
+                image: image,
+                title: title,
+                description: description,
               ),
             );
           } else if (tableName == "Natura") {
             return Container(
               margin: const EdgeInsets.only(right: 10),
               child: NatureCard(
-                location: e['location'] ?? localizations.titleNotAvailable,
-                image: client.storage.from(tableName).getPublicUrl(e['image']),
-                title: e['title'] ?? localizations.titleNotAvailable,
+                location: location,
+                image: image,
+                title: title,
               ),
             );
           } else if (tableName == "Borghi") {
             return Container(
               margin: const EdgeInsets.only(right: 10),
               child: CityCard(
-                description:
-                    e['description'] ?? localizations.descriptionNotAvailable,
-                image: client.storage.from(tableName).getPublicUrl(e['image']),
-                title: e['title'] ?? localizations.titleNotAvailable,
+                description: description,
+                image: image,
+                title: title,
               ),
             );
           }
@@ -103,8 +106,9 @@ class _SearchPageState extends State<SearchPage> {
         setState(() {
           fetchedData[tableName] = widgetGenerated;
         });
-      } catch (e) {
+      } catch (e, stacktrace) {
         print("Errore nella fetch della tabella $tableName: $e");
+        print(stacktrace);
       }
     }
 
@@ -116,22 +120,27 @@ class _SearchPageState extends State<SearchPage> {
   @override
   void initState() {
     super.initState();
-    // No fetchAllTables() call here
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     final localizations = AppLocalizations.of(context);
-    if (localizations != null) {
+    if (localizations != null && !isDataFetched) {
       fetchAllTables(localizations);
     }
   }
 
   String getRandomTitle(AppLocalizations localizations, String tableName) {
-    final randomIndex = Random().nextInt(3) + 1;
+    final randomIndex = Random().nextInt(3) + 1; // Genera numeri da 1 a 3
     final titleKey = '${tableName.toLowerCase()}Title$randomIndex';
-    return localizations.getString(titleKey) ?? localizations.titleNotAvailable;
+    final title = localizations.getString(titleKey);
+
+    // Debug prints per controllare la chiave e il valore del titolo
+    print('Fetching title with key: $titleKey');
+    print('Fetched title: $title');
+
+    return title ?? localizations.titleNotAvailable;
   }
 
   @override
